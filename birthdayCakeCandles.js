9478b3ee6d671890dcbbea6a626790bec900ae32
Kamlesh Kamble <kamlesh.kamble2.95@gmail.com>
2021-06-07 20:41:00 +0530

    Birthday Cake Candles

--- birthdayCakeCandles.js
+++ birthdayCakeCandles.js
@@ -1,16 +1,21 @@
 console.log("Birthday Cake Candles");
 
 function birthdayCakeCandles(candles) {
-    // Write your code here
-    let count = 0;
-    candles.sort(function (a, b) { return b-a})
-    for(let i = 0; i < candles.length; i++){
-        if(candles[0] === candles[i]){
-            count += 1;
-        }
-    }
-    return count;
+  // Write your code here
+
+  let max = Math.max(...candles);
+  let f = candles.filter((elm) => elm === max);
+  return f.length;
+
+  // let count = 0;
+  // candles.sort(function (a, b) { return b-a})
+  // for(let i = 0; i < candles.length; i++){
+  //     if(candles[0] === candles[i]){
+  //         count += 1;
+  //     }
+  // }
+  // return count;
 }
 
-let a = [3,2,1,3];
-console.log(birthdayCakeCandles(a))
+let a = [3, 2, 1, 3];
+console.log(birthdayCakeCandles(a));
